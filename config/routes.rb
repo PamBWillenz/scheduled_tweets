3cfb176c8f75807f8aa964e6821fecd974b14fce
Pam Willenz <pam@positek.net>
2021-04-10 13:21:34 -0400

    Add registrations controller, sign up form, routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # GET /about
   get "about", to: "about#index"
+
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+  
   root to: "main#index"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

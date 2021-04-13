adcc607c14ca273f891c74815cda3c0f3859755e
Pam Willenz <pam@positek.net>
2021-04-13 16:10:39 -0400

    Add tweet model, view index new, controller

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,17 @@
+class TweetsController < ApplicationController
+  before_action :require_user_logged_in!
+
+  def index
+    @tweets = Current.user.tweets
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+end
